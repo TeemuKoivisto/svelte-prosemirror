b0761878b916aefd7a4cd51711fde9da9d487bc7
TeemuKoivisto <TeemuKoivisto@users.noreply.github.com>
2023-04-07 16:16:43 +0000

    deploy: ecd79b9a6f43868d21d4f9371f31c2fd62c31931

---
+++ _app/immutable/chunks/0.2c466045.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout.4039aba4.js";import{default as t}from"../entry/_layout.svelte.1d44b441.js";export{t as component,r as universal};
---
+++ _app/immutable/entry/_layout.ts.8a0e1e76.js
@@ -0,0 +1,1 @@
+import{p}from"../chunks/_layout.4039aba4.js";export{p as prerender};
